e84b5b795bf5f092037663d803d9d22e02a01df0
Movsar Makhmutov <movsar.makhmutov@auriga.com>
2021-05-07 03:24:40 +0300

    remove Nth node from end of list

---
+++ removeNthNodeFromEndOfList.ts
@@ -0,0 +1,40 @@
+// @ts-ignore
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+  let dummy = new ListNode(0);
+  dummy.next = head;
+
+  let p1: ListNode | null = dummy;
+  let p2: ListNode | null = dummy;
+
+  for (let i = 0; i <= n; i++) {
+    if (p2) p2 = p2.next;
+  }
+
+  while (p2) {
+    p1 = p1!.next;
+    p2 = p2.next;
+  }
+
+  p1!.next = p1!.next!.next;
+  return dummy.next;
+}
+
+removeNthFromEnd(
+  {
+    val: 1,
+    next: {
+      val: 2,
+      next: { val: 3, next: { val: 4, next: { val: 5, next: null } } },
+    },
+  },
+  2
+);
