1f12eb62fd108f1085ef950a1000a33bd51d564f
Movsar Makhmutov <movsar.makhmutov@auriga.com>
2021-05-07 03:28:10 +0300

    merge k sorted lists

---
+++ mergeKSortedLists.ts
@@ -0,0 +1,46 @@
+// @ts-ignore
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
+  if (lists.length === 1) return lists[0];
+  if (lists.length === 0) return null;
+  let head = joinLists(lists[0], lists[1]);
+
+  for (let i = 2; i < lists.length; i++) {
+    head = joinLists(head, lists[i]);
+  }
+  return head;
+}
+
+function joinLists(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  let dummyNode = new ListNode(0);
+  let result = dummyNode;
+
+  while (l1 !== null && l2 !== null) {
+    if (l1.val < l2.val) {
+      dummyNode.next = l1;
+      dummyNode = dummyNode.next;
+      l1 = l1.next;
+    } else {
+      dummyNode.next = l2;
+      dummyNode = dummyNode.next;
+      l2 = l2.next;
+    }
+  }
+
+  l1 === null ? (dummyNode.next = l2) : (dummyNode.next = l1);
+  return result.next;
+}
+
+mergeKLists([
+  { val: 2, next: { val: 4, next: { val: 6, next: null } } },
+  { val: 1, next: { val: 3, next: { val: 5, next: null } } },
+  { val: 2, next: { val: 6, next: null } },
+]);
